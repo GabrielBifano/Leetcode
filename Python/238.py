2031b3599ef6e55278dcb556020f5678e1fb7cc8
GabrielBifano <gabrielfev@outlook.com>
2023-10-25 14:50:06 -0300

    Product of Array Except Self in python

---
+++ Python/238.py
@@ -0,0 +1,19 @@
+# Product of Array Except Self
+# Medium
+
+def productExceptSelf(nums: list[int]) -> list[int]:
+    
+    cumulative_prefix = 1
+    cumulative_suffix  = 1
+
+    siz = len(nums)
+    mul_values = [0]*siz
+    for i in range(siz):
+        mul_values[i] = cumulative_prefix
+        cumulative_prefix *= nums[i]
+    
+    for i in range(siz - 1, -1, -1):
+        mul_values[i] *= cumulative_suffix
+        cumulative_suffix *= nums[i]
+    
+    return mul_values
