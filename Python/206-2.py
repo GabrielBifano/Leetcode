862811a038a86614fc5493a2849cd522b5bc5f6b
GabrielBifano <gabrielfev@outlook.com>
2023-11-07 21:18:47 -0300

    Reverse Linked List in python

---
+++ Python/206-2.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def change_values(head, cum: 'list[int]'):
+    
+    cum.append(head.val)
+    if head.next == None:
+        head.val = cum[0]
+        return (head, 1)
+    else:
+        _, i = change_values(head.next, cum)
+        head.val = cum[i]
+        return (head, i + 1)
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    
+    if head == None: return None
+    else: 
+        h, _ = change_values(head, [])
+        return h
