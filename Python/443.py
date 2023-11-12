26b3e18126e059fd9a0984f555b2e8819bab6161
GabrielBifano <gabrielfev@outlook.com>
2023-11-12 14:09:41 -0300

    String Compression in python

--- Python/443.py
+++ Python/443.py
@@ -1,25 +1,33 @@
 def compress(chars: 'list[str]') -> int:
 
         sentence = "".join(chars)
-        store = []
-        store_siz = 0
+        buff = []
+        buffsiz = 0
+
         while len(sentence) > 0:
-            size_before = len(sentence)
-            char = sentence[0]
-            sentence = sentence.lstrip(char)
-            diff = size_before - len(sentence)
-            store.append((char, diff))
-            store_siz += 1 + len(str(diff))
+            # saves old str length
+            length = len(sentence)
+
+            # apply lstrip
+            sentence = sentence.lstrip(sentence[0])
+            
+            # get the repetitions of first character
+            diff = length - len(sentence)
+
+            # store the character and its repetitions
+            buff.append((sentence[0], diff))
+
+            # updates the space that buff will occupy later in chars
+            buffsiz += 1 + len(str(diff)) * (diff > 1)
         
+        # write buff to chars
         j = 0
-        for char, reps in store:
+        for char, reps in buff:
             chars[j] = char
             if reps > 1:
                 for alg in str(reps):
                     j += 1
                     chars[j] = alg
             j += 1
-
-        return store_siz
-    
-print(compress(["a","a","b","b","c","c","c"]))
+        
+        return buffsiz
