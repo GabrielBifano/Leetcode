5da4ea7de8aa94440eebb4b919cffc79e27331cd
GabrielBifano <gabrielfev@outlook.com>
2023-11-17 12:10:42 -0300

    Count Good Nodes in Binary Tree in python second solution sucint

---
+++ Python/1448-2.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def goodNodes(root: TreeNode) -> int:
+    
+    def traverse(node: TreeNode, maxNode: int) -> int:
+        
+        if not node: return 0
+        elif node.val >= maxNode:
+            return 1 + traverse(node.left, node.val) + traverse(node.right, node.val)
+        else:
+            return traverse(node.left, maxNode) + traverse(node.right, maxNode)
+    
+    return traverse(node=root, maxNode=root.val)
