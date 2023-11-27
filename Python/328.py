02110d74593234e66a00ce56d30c08e88d3689c3
GabrielBifano <gabrielfev@outlook.com>
2023-11-27 19:21:09 -0300

    Odd Even Linked List

---
+++ Python/328.py
@@ -0,0 +1,31 @@
+
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def oddEvenList(head: Optional[ListNode]) -> Optional[ListNode]:
+
+    if not head or not head.next: return head
+
+    oddhead = head
+    bp = evenhead = head.next
+
+    turn = 3
+    node = evenhead.next
+    while node:
+        if turn % 2 != 0:
+            oddhead.next = node
+            oddhead = node
+        else:
+            evenhead.next = node
+            evenhead = node
+        node = node.next
+        turn += 1
+
+    oddhead.next = bp
+    evenhead.next = None
+    return head
