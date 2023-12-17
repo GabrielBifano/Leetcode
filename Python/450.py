b76e02e922b8f7a084c3cb8d9921dcae99bdc9ec
GabrielBifano <gabrielfev@outlook.com>
2023-12-17 15:30:42 -0300

    Delete Node in a BST in python

--- Python/450.py
+++ Python/450.py
@@ -13,13 +13,16 @@ class TreeNode:
 
 def deleteNode(root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         
-    def get_node(node, key, parent=None) -> Optional[tuple]:
-        if not node: return None
-        elif key < node.val: get_node(node.left,  key, node)
-        elif key > node.val: get_node(node.right, key, node)
+    def get_node(node, key, parent=None):
+        if node == None: return None
+        elif key < node.val: return get_node(node.left,  key, node)
+        elif key > node.val: return get_node(node.right, key, node)
         else: return (node, parent)
-    
+        
     def del_node(node, parent) -> None:
+        if parent is None:
+            node = None
+            return 
         if parent.left == node:
             if node.right is not None:
                 parent.left = node.right
@@ -30,7 +33,7 @@ def deleteNode(root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
             else: parent.right = node.left
 
     def del_twin_child_node(node) -> None:
-        sparent = None
+        sparent = node
         succ = node.right
         while True:
             if succ.left is not None:
@@ -44,74 +47,11 @@ def deleteNode(root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
     if t is None: return root
     
     remove_me, parent = t
-    remove_me.val = 100
-    
-    # elif remove_me.left is None and remove_me.right is None:
-    #     del_node(remove_me, parent)
-    # elif remove_me.left is not None and remove_me.right is not None:
-    #     del_twin_child_node(remove_me)
-    # else:
-    #     del_node(remove_me, parent)
-
-    return root
-
-
-
-
-
-
-
-
-
-
-
-def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-    self.actual_node_found = None
-    
-    def find_val(node, key):
-        if node == None:
-            return node
-
-        if node.val == key:
-            if node.right and node.left:
-                self.actual_node_found = node
-            if node.right:
-                return node.right
-            else:
-                return node.left
-            return None
-        
-        node.left = find_val(node.left, key)
-        node.right = find_val(node.right, key)
-
-        return node
-
-    root = find_val(root, key)
-    
-    if self.actual_node_found is None or \
-    self.actual_node_found.left is None:
-        return root
-
-    #now the problem becomes of attaching left subtree of found val to this right child of the found val
-
-    #how to do this 
-
-    right = self.actual_node_found.right
-    left = self.actual_node_found.left
-
-    while True:
-        prev = right
-        if right.val > left.val:
-            right = right.left
-        else:
-            right = right.right
-
-        if right == None:
-            break
-    
-    if prev.val > left.val:
-        prev.left = left
+    if   remove_me.left is None and remove_me.right is None:
+        del_node(remove_me, parent)
+    elif remove_me.left is not None and remove_me.right is not None:
+        del_twin_child_node(remove_me)
     else:
-        prev.right = left
+        del_node(remove_me, parent)
 
     return root
