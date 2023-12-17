cd7a6657c8e60d2960c3d605d053904f77ed7c9f
GabrielBifano <gabrielfev@outlook.com>
2023-12-17 12:29:37 -0300

    Number of province in python

---
+++ Python/547.py
@@ -0,0 +1,25 @@
+# Number of Provinces
+
+class Solution:        
+
+    def dfs(self, node, isConnected, visited):
+        visited.add(node)
+        for i in range(len(isConnected)):
+            if isConnected[node][i] == 1 and i not in visited:
+                self.dfs(i, isConnected, visited)
+
+    def findCircleNum(self, isConnected: 'list[list[int]]') -> int:
+
+        p = 0
+        width = len(isConnected)
+        visited = set()
+
+        while len(visited) != width:
+            node = 0
+            for i in range(width):
+                if i not in visited:
+                    node = i
+            self.dfs(node, isConnected, visited)                        
+            p += 1        
+        return p
+                
