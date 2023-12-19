27b2c7459a87935b591c8ecef195398e55435e4b
GabrielBifano <gabrielfev@outlook.com>
2023-12-19 10:08:40 -0300

    Reorder Routes to Make All Paths Lead to the City Zero in python

---
+++ Python/1466.py
@@ -0,0 +1,28 @@
+# Reorder Routes to Make All Paths Lead to the City Zero
+# Medium
+
+class Solution:
+    def minReorder(self, n: int, connections: 'list[list[int]]') -> int:
+        
+        self.flips = 0
+        
+        def dfs(city: int, visited):
+            
+            neighbours = [vertex for vertex in connections if city in vertex]
+            for nei in neighbours:
+                
+                if tuple(nei) in visited: continue
+                start, finish = nei[0], nei[1]
+
+                if finish == city:
+                    visited.add(tuple(nei))
+                    dfs(start, visited)
+                
+                elif start == city:
+                    self.flips += 1
+                    visited.add(tuple(nei))
+                    dfs(finish, visited)
+
+        
+        dfs(0, set())
+        return self.flips
